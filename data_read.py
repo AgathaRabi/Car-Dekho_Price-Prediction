c0ab1eecd3066c4e76d26acd9199e1c09ea49784
AGATHA RABI <agatharabij@gmail.com>
2025-06-03 23:43:59 +0530

    Data read

---
+++ data_read.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# Read the excel files
+kolkata_data = pd.read_excel("C:\\Users\\PAPPILON\\Downloads\\kolkata_cars.xlsx")
+jaipur_data = pd.read_excel("C:\\Users\\PAPPILON\\Downloads\\jaipur_cars.xlsx")
+#print(kolkata_data)
+
+
+
